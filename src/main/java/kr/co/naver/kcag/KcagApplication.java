2a97d89ad51c9fcecd607612d2db73c07fa132fd
semistone222 <semistone222@gmail.com>
2017-11-19 16:02:36 +0900

    Initial commit

---
+++ src/main/java/kr/co/naver/kcag/KcagApplication.java
@@ -0,0 +1,12 @@
+package kr.co.naver.kcag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcagApplication.class, args);
+	}
+}
